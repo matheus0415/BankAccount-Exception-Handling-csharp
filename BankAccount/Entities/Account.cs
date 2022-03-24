35bbde9989d1d96126265450c3983bee9e1f2319
Matheus Santos <somathsant@gmail.com>
2022-03-23 21:32:33 -0300

    First Commit

---
+++ BankAccount/Entities/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BankAccount.Entities.Exceptions;
+
+namespace BankAccount.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+        public void Deposit(double amount)
+        {   
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {   
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainException("Exceeded withdraw amount limit!");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Insufficent funds");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BankAccount.Entities;
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine());
+
+
+                Account account0 = new Account(number, name, initialBalance, withdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                
+                account0.Withdraw(amount);
+
+                Console.WriteLine("New balance: " + account0.Balance);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
